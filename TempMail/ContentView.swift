4fa2e7b053e1d362018929bd1cba29a5645f412e
Aravind Koneru <aravind.b.koneru@gmail.com>
2020-05-25 13:11:51 -0400

    Initial Commit

---
+++ TempMail/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TempMail
+//
+//  Created by Aravind Koneru on 5/25/20.
+//  Copyright © 2020 Aravind Koneru. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
