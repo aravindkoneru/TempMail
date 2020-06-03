aed7c0e4e0b55c9fe2287af755b65a5b099f3576
Aravind Koneru <aravind.b.koneru@gmail.com>
2020-06-03 16:44:43 -0400

    change layout based on device orientation

--- TempMail/ContentView.swift
+++ TempMail/ContentView.swift
@@ -8,46 +8,108 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
-    
+    @State var orientation: UIDeviceOrientation = UIDevice.current.orientation
     var email_addr: String
     
+    // TODO: can the header be abstracted? lots of repeated code...
+    
     var body: some View {
-        VStack {
-            Text("Temp Mail")
-                .font(.largeTitle)
-                .padding()
-            Spacer()
-            
-            Button(action: {
-                print("generate new email addr")
-            }) {
-                Text("Generate New Email")
-                    .fontWeight(.bold)
-                    .foregroundColor(.blue)
-                    .padding()
-                    .overlay(RoundedRectangle(cornerRadius: 40)
-                        .stroke(Color.blue, lineWidth: 5))
+        return HStack {
+            if orientation.isPortrait {
+                VStack {
+                    Text("Temp Mail")
+                        .font(.largeTitle)
+                        .padding()
+                    Spacer()
+                    
+                    Button(action: {
+                        print("generate new email addr")
+                    }) {
+                        Text("Generate New Email")
+                            .fontWeight(.bold)
+                            .foregroundColor(.blue)
+                            .padding()
+                            .overlay(RoundedRectangle(cornerRadius: 40)
+                                .stroke(Color.blue, lineWidth: 5))
+                    }
+                    
+                    Spacer()
+                    
+                    Button(action: {
+                        print("copy to clipboard")
+                        UIPasteboard.general.string = self.email_addr
+                        //                let copiedIcon = UIImage(systemName: "doc.on.clipboard")
+                    }) {
+                        Text("\(email_addr)")
+                            .padding()
+                            .foregroundColor(.black)
+                            .font(.title)
+                    }
+                    
+                    
+                    
+                    Spacer()
+                    
+                    List {
+                        MessageRow(messageInfo: InboxModel(id: 1234,
+                                                           from: "sender@example.com",
+                                                           subject: "example subject",
+                                                           date: "Today"))
+                        MessageRow(messageInfo: InboxModel(id: 1234,
+                                                           from: "sender@example.com",
+                                                           subject: "example subject",
+                                                           date: "Today"))
+                    }
+                                        
+                }
+            } else {
+                VStack {
+                    Text("Temp Mail")
+                        .font(.largeTitle)
+                        .padding()
+                    
+                    Button(action: {
+                        print("generate new email addr")
+                    }) {
+                        Text("Generate New Email")
+                            .fontWeight(.bold)
+                            .foregroundColor(.blue)
+                            .padding()
+                            .overlay(RoundedRectangle(cornerRadius: 40)
+                                .stroke(Color.blue, lineWidth: 5))
+                    }
+                    
+                    Button(action: {
+                        print("copy to clipboard")
+                        UIPasteboard.general.string = self.email_addr
+                        //                let copiedIcon = UIImage(systemName: "doc.on.clipboard")
+                    }) {
+                        Text("\(email_addr)")
+                            .padding()
+                            .foregroundColor(.black)
+                            .font(.title)
+                    }
+                }
+                
+                Spacer()
+                
+                List {
+                    MessageRow(messageInfo: InboxModel(id: 1234,
+                                                       from: "sender@example.com",
+                                                       subject: "example subject",
+                                                       date: "Today"))
+                    MessageRow(messageInfo: InboxModel(id: 1234,
+                                                       from: "sender@example.com",
+                                                       subject: "example subject",
+                                                       date: "Today"))
+                }
+                
             }
-            
-            Spacer()
-            
-            Text("Current Address: \(email_addr)")
-                .padding()
-            
-            Spacer()
-            
-            List {
-                MessageRow(messageInfo: InboxModel(id: 1234,
-                                                   from: "sender@example.com",
-                                                   subject: "example subject",
-                                                   date: "Today"))
-                MessageRow(messageInfo: InboxModel(id: 1234,
-                                                   from: "sender@example.com",
-                                                   subject: "example subject",
-                                                   date: "Today"))
-            }
-            
+        }
+        .onReceive(NotificationCenter.Publisher(center: .default, name: UIDevice.orientationDidChangeNotification)) {_ in
+            self.orientation = UIDevice.current.orientation
         }
     }
 }
