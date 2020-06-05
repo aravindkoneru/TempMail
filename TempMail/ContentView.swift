87054726eea75146bd917fe84c8f263395f9069a
paulmjojy <34897920+paulmjojy@users.noreply.github.com>
2020-06-05 15:26:28 -0400

    Making an Email Class (#3)
    * Generate New Email button now has functionality
    
    * added some comments
    
    * Changed the Email Struct to a class
    
    * updated with pull request suggestions
    
    * update to pull request comments
    
    * internal update still working on pull request
    
    * Updated the method sig for genEmailAddr, and tweaked the getter for the email address
    
    * Final update

--- TempMail/ContentView.swift
+++ TempMail/ContentView.swift
@@ -9,18 +9,21 @@
 import SwiftUI
 
 struct ContentView: View {
-    
-    var email_addr: String
+//    variable for Email struct
+    @ObservedObject var email: Email = Email()
+    //this variable is for later, when I implement a feature that will save user data even when the app is closed
+    private let defaults = UserDefaults.standard
     
     var body: some View {
         VStack {
+//          title
             Text("Temp Mail")
                 .font(.largeTitle)
                 .padding()
             Spacer()
-            
+//          Generate new email button
             Button(action: {
-                print("generate new email addr")
+                self.email.setNewEmailAddr()
             }) {
                 Text("Generate New Email")
                     .fontWeight(.bold)
@@ -31,12 +34,12 @@ struct ContentView: View {
             }
             
             Spacer()
-            
-            Text("Current Address: \(email_addr)")
+//          display email address for user
+            Text("Current Address: \(self.email.getEmailAddr())@1secmail.com")
                 .padding()
             
             Spacer()
-            
+//          List of emails attached to above address
             List {
                 MessageRow(messageInfo: InboxModel(id: 1234,
                                                    from: "sender@example.com",
@@ -54,6 +57,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView(email_addr: "bob@example.com")
+        ContentView()
     }
 }
