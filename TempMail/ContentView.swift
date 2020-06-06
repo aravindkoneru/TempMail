3cfff538b5df2df1fa6435f642d34289de4af7b7
Aravind Koneru <aravind.b.koneru@gmail.com>
2020-06-06 10:50:32 -0400

    added navigation support for clicking on inbox rows and addressed some pr feedback

--- TempMail/ContentView.swift
+++ TempMail/ContentView.swift
@@ -10,108 +10,50 @@ import SwiftUI
 
 
 struct ContentView: View {
-    @State var orientation: UIDeviceOrientation = UIDevice.current.orientation
-//    variable for Email struct
     @ObservedObject var email: Email = Email()
-    //this variable is for later, when I implement a feature that will save user data even when the app is closed
+    //TODO: this variable is for later, when I implement a feature that will save user data even when the app is closed
     private let defaults = UserDefaults.standard
     
-    // TODO: can the header be abstracted? lots of repeated code...
     var body: some View {
-        return HStack {
-            if orientation.isPortrait || orientation.isFlat { // TODO: test more to ensure that this is desired behavior
-                VStack {
-                    Text("Temp Mail")
-                        .font(.largeTitle)
+        NavigationView {
+            VStack {
+                Text("Temp Mail")
+                    .font(.largeTitle)
+                    .padding()
+                
+                Button(action: {
+                    print("copy to clipboard")
+                    UIPasteboard.general.string = "\(self.email.getEmailAddr())@1secmail.com"
+                }) {
+                    Text("\(self.email.getEmailAddr())@1secmail.com")
                         .padding()
-                    Spacer()
-                    
-                    Button(action: {
-                        self.email.setNewEmailAddr()
-                    }) {
-                        Text("Generate New Email")
-                            .fontWeight(.bold)
-                            .foregroundColor(.blue)
-                            .padding()
-                            .overlay(RoundedRectangle(cornerRadius: 40)
-                                .stroke(Color.blue, lineWidth: 5))
-                    }
-                    
-                    Spacer()
-                    
-                    Button(action: {
-                        print("copy to clipboard")
-                        UIPasteboard.general.string = "\(self.email.getEmailAddr())@1secmail.com"
-                    }) {
-                        Text("\(self.email.getEmailAddr())@1secmail.com")
-                            .padding()
-                            .foregroundColor(.black)
-                            .font(.title)
-                    }
-                    
-                    
-                    
-                    Spacer()
-                    
-                    List {
-                        MessageRow(messageInfo: InboxModel(id: 1234,
-                                                           from: "sender@example.com",
-                                                           subject: "example subject",
-                                                           date: "Today"))
-                        MessageRow(messageInfo: InboxModel(id: 1234,
-                                                           from: "sender@example.com",
-                                                           subject: "example subject",
-                                                           date: "Today"))
-                    }
-                                        
-                }
-            } else {
-                VStack {
-                    Text("Temp Mail")
-                        .font(.largeTitle)
-                        .padding()
-                    
-                    Button(action: {
-                        self.email.setNewEmailAddr()
-                    }) {
-                        Text("Generate New Email")
-                            .fontWeight(.bold)
-                            .foregroundColor(.blue)
-                            .padding()
-                            .overlay(RoundedRectangle(cornerRadius: 40)
-                                .stroke(Color.blue, lineWidth: 5))
-                    }
-                    
-                    Button(action: {
-                        print("copy to clipboard")
-                        UIPasteboard.general.string = "\(self.email.getEmailAddr())@1secmail.com"
-                    }) {
-                        Text("\(self.email.getEmailAddr())@1secmail.com")
-                            .padding()
-                            .foregroundColor(.black)
-                            .font(.title)
-                    }
+                        .foregroundColor(.black)
+                        .font(.title)
                 }
                 
                 Spacer()
                 
-                List {
-                    MessageRow(messageInfo: InboxModel(id: 1234,
-                                                       from: "sender@example.com",
-                                                       subject: "example subject",
-                                                       date: "Today"))
-                    MessageRow(messageInfo: InboxModel(id: 1234,
-                                                       from: "sender@example.com",
-                                                       subject: "example subject",
-                                                       date: "Today"))
+                Button(action: {
+                    self.email.setNewEmailAddr()
+                }) {
+                    Text("Generate New Email")
+                        .fontWeight(.bold)
+                        .foregroundColor(.blue)
+                        .padding()
+                        .overlay(RoundedRectangle(cornerRadius: 40)
+                            .stroke(Color.blue, lineWidth: 5))
+                }.padding()
+                
+                
+                List(self.email.getInbox()) { message in
+                    NavigationLink(destination: MessageView(message: self.email.getMessageContent(id: message.id))) {
+                        InboxRow(messageInfo: message)
+                    }
                 }
+                .navigationBarHidden(true)
+                .navigationBarTitle("", displayMode: .inline)
                 
             }
-        }
-        .onReceive(NotificationCenter.Publisher(center: .default, name: UIDevice.orientationDidChangeNotification)) {_ in
-            self.orientation = UIDevice.current.orientation
-            print("portrait?: \(self.orientation.isPortrait)")
-            print("flat?: \(self.orientation.isFlat)\n")
         }
     }
 }
