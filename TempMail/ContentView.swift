17a05c795a87ba083cbf883dcff40caec4406a9e
paulmjojy <34897920+paulmjojy@users.noreply.github.com>
2020-06-25 16:35:30 -0400

    Added Functionality to Inbox (#5)
    * Generate New Email button now has functionality
    
    * added some comments
    
    * Changed the Email Struct to a class
    
    * updated with pull request suggestions
    
    * update to pull request comments
    
    * internal update still working on pull request
    
    * Updated the method sig for genEmailAddr, and tweaked the getter for the email address
    
    * Final update
    
    * App will know fetch the inbox from the server
    
    * Update to init method of Email to include the inbox var
    
    * Updated the Email file to remove the guard and the request wrapper. Also removed the autogenerated struct stub, and a do catch to the decoder.
    
    * Abstracted the JSON Decode and sending part to its own method and increased the timer to 30 sec
    
    * Abstracted the decoder for error handling and made the URL a URLComponents object
    
    * made parseInbox a private func
    
    * made loadInbox a private function as well
    
    * changed timer back to 30sec for refresh and changed email from "test" to the email_addr instance variable
    
    * added a cache check to loadInbox and inbox resets when setNewEmailAddr is called
    
    * Added a reset function, made instance variables optionals, changed the button to call reset when pressed, moved if conditional out of the main thread, changed url to string url
    
    * Changed setNewEmailAddr() func to get, made the email addr not an optional val, fixed formatting, moved loadInbox() to init
    
    * changed func signature of setNewEmailAddr, to genNewEmailAddr
    
    * Removed self.inbox = nil from init because it is already nil as an optional val

--- TempMail/ContentView.swift
+++ TempMail/ContentView.swift
@@ -34,7 +34,8 @@ struct ContentView: View {
                 Spacer()
                 
                 Button(action: {
-                    self.email.setNewEmailAddr()
+                    self.email.reset()
+                    
                 }) {
                     Text("Generate New Email")
                         .fontWeight(.bold)
--- TempMail/utils/Email.swift
+++ TempMail/utils/Email.swift
@@ -11,42 +11,76 @@ import SwiftUI
 
 class Email : ObservableObject{
     @Published var email_addr: String
+    @Published var inbox: [InboxModel]?
     
     //  init method
     init() {
+        //need to initialize email_addr to an empty string first to avoid the error: "self" used in method call before all stored properties are initialized
         self.email_addr = ""
-        setNewEmailAddr()
+        self.email_addr = genNewEmailAddr()
+        loadInbox()
     }
+    
+    //resets the email
+    func reset() -> Void {
+        self.inbox = nil
+        self.email_addr = genNewEmailAddr()
+    }
+    
     //  generates an email address
-    func setNewEmailAddr() -> Void {
+    func genNewEmailAddr() -> String {
         //list of acceptable chars
         let letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-        //      returns a random permutation of letters; string of up to length 9       for email addr
-        self.email_addr = String((0..<9).map{ _ in letters.randomElement()!})
+        //      returns a random permutation of letters; string of up to length 8 for email addr
+        return String((0..<8).map{ _ in letters.randomElement()!})
+    }
+    
+    // loads the Inbox that corresponds to the email address from the server, refreshing every second
+    private func loadInbox() -> Void {
+        //creates the timer to refresh every 30 seconds and run the code in the block
+        Timer.scheduledTimer(withTimeInterval: 30, repeats: true) { timer in
+            let email_addr = self.email_addr
+            //creates the url that will make the API call
+            guard let url = URL(string: "https://www.1secmail.com/api/v1/?action=getMessages&login=\(self.email_addr)&domain=1secmail.com") else {fatalError("Invalid URL")}
+            //makes the request to server
+            URLSession.shared.dataTask(with: url) { (data, response, error) in
+                if let error = error {
+                    print("Error: \(error)")
+                    return
+                }
+                guard let data = data else {return}
+                guard let inbox = self.parseInboxData(data: data) else {return}
+                //updates instance variable
+                DispatchQueue.main.async {
+                    if self.email_addr == email_addr {
+                        self.inbox = inbox
+                    }
+                }
+            }.resume()
+        }
+    }
+    
+    //parses Inbox JSON data
+    private func parseInboxData(data: Data) -> [InboxModel]? {
+        var response: [InboxModel]?
+        do {
+            response = try JSONDecoder().decode([InboxModel].self, from: data)
+        } catch {
+            print("Fetch failed: \(error.localizedDescription)")
+        }
+        return response
     }
     
     //  getter for email addr
     func getEmailAddr() -> String {
         return email_addr
     }
+    
     //  getter for emails
-    func getEmails() {
-        //        make the HTTP GET request to return the JSON shit
+    func getInbox() -> [InboxModel]? {
+        return inbox
     }
     
-    // @TODO: implement real functionality
-    // gets all of the message meta data for this email address
-    func getInbox() -> [InboxModel] {
-        return [
-            InboxModel(id: 1234,
-                       from: "sender@example.com",
-                       subject: "example subject",
-                       date: "Today"),
-            InboxModel(id: 1234,
-                       from: "sender@example.com",
-                       subject: "example subject",
-                       date: "Today")]
-    }
     
     // @TODO: implement real functionality
     // gets the contents of an individual message
